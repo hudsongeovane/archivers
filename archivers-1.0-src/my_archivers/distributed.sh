4578f9280412baa1b1ada8f7bcef29dd2d8f0135
hudsongeovane <hudsongeovane@gmail.com>
2013-12-05 17:21:26 -0300

    Dia 05-12-2013

--- archivers-1.0-src/my_archivers/distributed.sh
+++ archivers-1.0-src/my_archivers/distributed.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-FILE="teste/seq-smallPF-2d-10000.txt"
+FILE="teste/seq-smallPF-3d-10000.txt"
 for (( c=1; c<=20; c++ ))
 do
-./archive $FILE distributed -n 100 -hide -trash
+./archive $FILE distributed -n 100 -hide
 done
